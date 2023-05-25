2a34de70f21e7c1beb7764b2bc1ff7b0839d68a4
easyhee <jihee001004@gmail.com>
2023-05-25 20:59:15 +0900

    에러안뜸

---
+++ ggmapping.R
@@ -0,0 +1,38 @@
+install.packages("ggmap", INSTALL_opts = c('--no-lock'))
+install.packages("ggplot2")
+install.packages("raster")
+install.packages("rgeos")
+install.packages("maptools")
+install.packages("rgdal")
+install.packages('sf')
+install.packages('mapproj')
+
+library(ggmap)
+library(ggplot2)
+library(raster)
+library(rgeos)
+library(maptools)
+library(rgdal)
+library(tidyverse)
+library(sf)
+library(mapproj)
+
+map_korea <-st_read('C:/data/preprocessed/EMD_202302/emd.shp')
+
+map_korea$DONG_CD <- iconv(map_korea$DONG_CD,
+                             from='CP949',
+                             to='UTF-8')
+P <- read.csv("C:/data/preprocessed/data_final.csv", header = TRUE) #시각화할 데이터셋
+map <- shapefile("C:/data/preprocessed/EMD_202302/emd.shp") #지리 정보 데이터셋
+
+map <- spTransform(map, CRSobj = CRS('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'))
+new_map <- fortify(map, region = 'DONG_CD')
+View(new_map)
+
+new_map$id <- as.numeric(new_map$id)
+seoul_map <- new_map[new_map$id <= 11740,]
+P_merge <- merge(seoul_map, P, by='id')
+                 
+ggplot() + geom_polygon(data = P_merge, aes(x=long, y=lat, group=group), fill = 'white', color='black')
+ggplot() + geom_polygon(data = P_merge, aes(x=long, y=lat, group=group, fill = A))
+
