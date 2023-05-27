86cb05abac22fb157142d832fd0f5d24fa16c19f
easyhee <jihee001004@gmail.com>
2023-05-28 04:16:55 +0900

    id 지정에 DONG_CD가 와야함

--- emd_convert_csv.R
+++ emd_convert_csv.R
@@ -2,7 +2,6 @@ library(ggplot2)
 library(dplyr)
 library(rgdal)
 library(sp)
-#완성
 
 from_crs <- CRS("+proj=tmerc +lat_0=38 +lon_0=127.5 +k=0.9996 +x_0=1000000 +y_0=2000000 +ellps=GRS80 +units=m")
 to_crs <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
@@ -34,6 +33,7 @@ head(shp_result)
 #위에서 만든 data_result에서 동명 추출 및 기준id 생성 
 Name <- cbind(id=row.names(data_result), emdname=as.character(data_result$EMD_ENG_NM))
 str(Name);head(Name)
+
 #list를 data.frame으로 변경 후, id를 factor->char로 변경 
 x <- as.data.frame(Name)
 x$id <- as.character(x$id)
@@ -45,3 +45,21 @@ str(result)
 head(result)
 #csv로 저장
 write.csv(result, file="C:/data/preprocessed/EMD_202101/TL_SCCO_EMD_convert.csv", row.names = F)
+
+#지도 그리기
+library(ggmap)
+library(ggplot2)
+library(grid)
+library(raster)
+library(rgeos)
+library(MASS)
+library(maptools)
+library(dplyr)
+library(rgdal)
+library(sp)
+
+P <- read.csv("C:/data/preprocessed/data_final.csv")
+map <- read.csv("C:/data/preprocessed/EMD_202101/TL_SCCO_EMD_convert.csv")
+
+new_map <- fortify(shp_result, x)
+View(new_map) #id에 동별 코드가 들어가면 끝날거 같은데 어떻게 해야할지 모르겠음
