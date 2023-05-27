0cce58eaa70287a3d11bbed5ee6f29d69eb28242
easyhee <jihee001004@gmail.com>
2023-05-28 03:29:25 +0900

    이걸 쓰면 shp를 csv파일 변환되는데, 위도 경도 좌표 값이 나타남

--- emd_convert_csv.R
+++ emd_convert_csv.R
@@ -2,7 +2,7 @@ library(ggplot2)
 library(dplyr)
 library(rgdal)
 library(sp)
-
+#완성
 
 from_crs <- CRS("+proj=tmerc +lat_0=38 +lon_0=127.5 +k=0.9996 +x_0=1000000 +y_0=2000000 +ellps=GRS80 +units=m")
 to_crs <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
