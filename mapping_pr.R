d9adbbe88ccfd00c5a85c3afe318abb6b67c440b
easyhee <jihee001004@gmail.com>
2023-06-11 01:19:10 +0900

    일단 코드 푸쉬해여 ㅠ 지도가 화났음 ㅠㅠ

--- mapping_pr.R
+++ mapping_pr.R
@@ -8,54 +8,62 @@ library(maptools)
 library(rgdal)
 library(IRdisplay)
 
-## 행정동 지도 그리기 (map outline data)
-h <- rgdal::readOGR('C:/data/preprocessed/map/emd_mapdata.shp')
-h <- spTransform(h, CRS('+proj=longlat'))
-h_map <- fortify(h)
+## 전국 행정동 지도 그리기 (map outline data)
+h <- rgdal::readOGR('C:/data/preprocessed/map/emd_mapdata.shp') #shp파일 불러오기
+h <- spTransform(h, CRS('+proj=longlat')) #좌표계 설정
+h_map <- fortify(h) #데이터프레임으로 변경
 head(h_map)
-write.csv(h_map,"C:/data/preprocessed/map/emd_mapdata.csv")#h_map:위경도값 있음
+write.csv(h_map,"C:/data/preprocessed/map/emd_mapdata.csv") #h_map: 전국 위경도값 있음
 
+## 전국 행정동 지도그리기
 ggplot() + geom_polygon(data = h_map, aes(x = long, y = lat, group = group),
-                        fill = 'white', colour = 'black') ## 전국 읍동리 지도그리기
+                        fill = 'white', colour = 'black')
 h_map$id <- as.numeric(h_map$id) # id의 class가 character라서 numeric으로 변환
 hmap <- h_map[h_map$id <= 423 | h_map$id == 3478,] # id를 QGIS로 확인한 결과 0~423, 3478번이 서울시에 해당
 
+# 서울시 행정동 지도 그리기
 ggplot() + geom_polygon(data = hmap, aes(x = long, y = lat, group = group),
-                        fill = 'white', colour = 'black') ## 서울 행정동 지도
+                        fill = 'white', colour = 'black')
 ggplot() + geom_polygon(data = hmap, aes(x=long, y=lat, group=group, fill = A))
 
 # 매핑용 행정동 코드 불러오기
-list_dong <- read.csv( 'C:/data/preprocessed/data_dong.csv', header = TRUE, fileEncoding = "CP949", encoding = "UTF-8")
+#list_dong <- read.csv( 'C:/data/preprocessed/data_dong.csv', header = TRUE, fileEncoding = "CP949", encoding = "UTF-8")
 #list_dong$DONG_CD <- sapply(list_dong$DONG_CD, as.character )
-head(list_dong)
-cd_data <- df[, c(6,7)]
-cd_data$DONG_CD <- as.integer(cd_data$DONG_CD)
-head(cd_data)
-str(cd_data)
+#head(list_dong)
+#cd_data <- df[, c(6,7)]
+#cd_data$DONG_CD <- as.integer(cd_data$DONG_CD)
+#head(cd_data)
+#str(cd_data)
 
-#행정동별_위경도_좌표 불러오기
-map_data <- read.csv('C:/data/preprocessed/행정동별_위경도_좌표.csv', header=TRUE, fileEncoding = "CP949", encoding = "UTF-8")
-head(map_data)
 
-#행정동 코드랑 위도경도 좌표 합산
-data_join <- left_join(map_data, list_dong, by = c('DONG_NM' = 'DONG_NM'))
-str(data_join)
+#행정동 코드, 위도경도 좌표 불러오기
+map_data <- read.csv('C:/data/preprocessed/data_dong_mapping.csv', header=TRUE, fileEncoding = "CP949", encoding = "UTF-8")
+map_data_1 <- fortify(map_data)
+head(map_data_1)
 
 #시각화할 데이터 불러오기
-hmap <- read.csv('C:/data/preprocessed/emd_mapdata.csv') %>% as_tibble() %>%
-hmap_cluster <- read.csv('C:/data/preprocessed/cluster.csv') 
-head(hmap_cluster)
-hmap_testdata <- read.csv('C:/data/preprocessed/test_data.csv')
-head(hmap_testdata)
+hmap <- read.csv('C:/data/preprocessed/map/emd_mapdata.csv')
+hmap_cluster <- read.csv('C:/data/preprocessed/cluster.csv')
+cluster1 <- hmap_cluster %>%
+filter(cluster == 1) %>%
+dplyr::select(cluster, all_of(1))
+head(cluster)
+
+hmap_ttdata <- read.csv('C:/data/preprocessed/total_data.csv')
+head(hmap_ttdata)
 
 #시각화할 데이터와 data_join 병합하기
+merge_result <- merge(map_data_1, hmap_cluster, by ="DONG_CD")
+head(merge_result)
+merge_result2 <- merge(hmap, hmap_ttdata, by =' ')
 
 ### 클러스터링 시각화
-ggplot() + geom_polygon(data = hmap_cluster, aes(x = long, y = lat, group = group, fill = ...),
-                        color = 'gray') + scale_fill_gradient(low = "#FFCCFF", high = "#6600CC", 
-                                                              space = "Lab", guide = "colourbar") + ggtitle('클러스터링')
+ggplot() + geom_polygon(data = merge_result, aes(x = long, y = lat,  fill = A))
+#,
+#                       color = 'gray') + scale_fill_gradient(low = "#FFCCFF", high = "#6600CC", 
+#                                                            space = "Lab", guide = "colourbar") + ggtitle('클러스터링')
 ##테스트데이터 시각화
-ggplot() + geom_polygon(data = hmap_testdata, aes(x = long, y = lat, group = group, fill = ...),
+ggplot() + geom_polygon(data = merge_result2, aes(x = long, y = lat, group = group, fill = ...),
                         color = 'gray') + scale_fill_gradient(low = "#FFCCFF", high = "#6600CC", 
                                                               space = "Lab", guide = "colourbar") + ggtitle('test data')
 
